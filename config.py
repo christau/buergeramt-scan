f22178a9ca3212741d314fbb7997fe0854514bc5
Christian Taubenheim <christian@sedna.de>
2022-06-18 09:40:54 +0200

    Cleaned up config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-# insert url which lays under "Termin berlinweit suchen" - Right click to copy
-url = "enter-url-here"
-# Interval to reload the data
+# Interval to recheck for appointments
 interval_time = 180
+# your telegram bot's token
 token="your token"
 
